cf30ee5b7d6e7a743ec0fc1c4a268330f419a835
my name <im-homosapien>
2025-02-01 23:08:49 -0600

    Resettinnnnnng

---
+++ reset-password.py
@@ -0,0 +1,7 @@
+username = input("Enter username = ")
+password = input("Enter pass = ")
+
+if username == "Admin" and password == "Admin":
+    print("You can reset")
+else:
+    print("You cannnot reset")
