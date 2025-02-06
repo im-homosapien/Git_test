b50fda2db740e241992fef45e8a70853eeef8f3b
my name <im-homosapien>
2025-02-06 05:23:17 -0600

    REFACTORED RESET LOGIV

--- reset-password.py
+++ reset-password.py
@@ -1,7 +1,1 @@
-username = input("Enter username = ")
-password = input("Enter pass = ")
-
-if username == "Admin" and password == "Admin":
-    print("You can reset")
-else:
-    print("You cannnot reset")
+print("Reset password")
